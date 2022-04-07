3c6b520aab7de0c4580703d349d963fa652f83f3
Bingauui <87755350+Bingauui@users.noreply.github.com>
2022-04-07 14:23:24 +0800

    Update 发送接受数据.py

--- 发送接受数据.py
+++ 发送接受数据.py
@@ -3,8 +3,6 @@
 # bcdedit.exe /set testsigning off  开启签名
 #数据处理转发添加CRC验证
 import threading
-import socket
-import json
 import sys
 sys.path.append("C:\\Users\\Bing\\Documents\\Python") #改成放datastruct.py的文件夹
 from datastruct import *
@@ -18,7 +16,7 @@ def data():
     while True:
         stu = pDll.bReadFile()
         s = stu.READ_CONSTANT_FROCE_DATA.Magnitude1 >> 7
-        eff = ((stu.READ_CONSTANT_FROCE_DATA.Magnitude1<<8)+stu.READ_CONSTANT_FROCE_DATA.Magnitude)
+        eff = ((stu.READ_CONSTANT_FROCE_DATA.Magnitude1<<8)+stu.READ_CONSTANT_FROCE_DATA.Magnitude)#赛车类游戏力反馈目前都只使用这一个  恒定力 注意大小端转换 16位数据输出
         if(s): 
             eff -= 2200
         else:
